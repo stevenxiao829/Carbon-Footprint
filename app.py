42a2bbadae01387ddc3a69ad929edf1849a8217e
Abhiram <113409577+Controls-Cat5e@users.noreply.github.com>
2023-08-12 14:37:16 -0700

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ EMISSION_FACTORS = {
      ,"America": {
         "Transportation": 0.14,  # kgCO2/km
         "Electricity": 0.82,  # kgCO2/kWh
-        "Diet": 1.25,  # kgCO2/meal, 2.5kgco2/kg
+        "Diet": 2,  # kgCO2/meal, 2.5kgco2/kg
         "Waste": 0.1  # kgCO2/kg
     }
 }
